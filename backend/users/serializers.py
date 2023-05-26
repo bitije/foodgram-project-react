ceecbf3cc0eb0a0c36b2a218f883268fc7e65cd9
gloth <somov.feodor.mail@gmail.com>
2023-05-26 16:58:58 +0300

    is_subscribed_fix

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 
 User = get_user_model()
@@ -7,13 +7,23 @@ User = get_user_model()
 
 class UserSerializer(ModelSerializer):
 
+    is_subscribed = SerializerMethodField()
+
     class Meta:
         model = User
         fields = (
             'email',
             'id',
             'username',
-            # 'is_subscribed',
+            'is_subscribed',
             'first_name',
             'last_name',
         )
+
+    def get_is_subscribed(self, obj: User) -> bool:
+        user = self.context.get('request').user
+
+        if user.is_anonymous or (user == obj):
+            return False
+
+        return user.subscriptions.filter(author=obj).exists()
