b49254f948143db0c548225313174dac050a2eb4
gloth <somov.feodor.mail@gmail.com>
2023-05-27 19:52:11 +0300

    second_iteration_init

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fifu8=gs@0ahd*ms0w4hn9eqh#6^&a_7os89w=(w+%hvgjgq@a'
+SECRET_KEY = os.environ['SECRET_KEY_VARIABLE']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -153,7 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [(BASE_DIR / 'static/'), ]
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [(BASE_DIR / 'static/'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
