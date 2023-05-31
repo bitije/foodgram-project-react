86756c054e89d87d8f21d3f52c827633e0106e9e
gloth <somov.feodor.mail@gmail.com>
2023-05-31 21:52:00 +0300

    fixes0.2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY_VARIABLE')
-SECRET_KEY = 'django-insecure-fifu8=gs@0ahd*ms0w4hn9eqh#6^&a_7os89w=(w+%hvgjgq@a'
+SECRET_KEY = os.getenv('SECRET_KEY_VARIABLE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -173,9 +172,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [(BASE_DIR / 'static/'), ]
+# STATICFILES_DIRS = [(BASE_DIR / 'static/'), ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
