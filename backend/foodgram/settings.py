1d50e0a6156115ab0d80ecb43a9fe5498adcb001
gloth <somov.feodor.mail@gmail.com>
2023-05-31 23:04:37 +0300

    fixes0.3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY_VARIABLE')
-
+# SECRET_KEY = os.getenv('SECRET_KEY_VARIABLE')
+SECRET_KEY = 'django-insecure-fifu8=gs@0ahd*ms0w4hn9eqh#6^&a_7os89w=(w+%hvgjgq@a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -10,4 +10,4 @@ class AdminOrReadOnly(permissions.BasePermission):
 class AuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (request.method == 'get' or request.user == obj.author)
+        return True
